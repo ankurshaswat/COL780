6706acb50f5e8b9df81aedc4bf1dc516569a3f65
Shashwat Shivam <shivamshaswat@gmail.com>
2019-09-27 12:24:14 +0530

    Ignore

--- A2/code/v4.py
+++ A2/code/v4.py
@@ -1,12 +1,12 @@
 """
-Fill documentation for complete module
+Python script to generate panaroma from multiple images
 Written by ankurshaswat on 18/09/2019
 """
 
 import argparse
+import copy
 import itertools
 import os
-import copy
 
 import cv2
 import imutils
@@ -17,9 +17,10 @@ print('Is CV3 Or Better = ', imutils.is_cv3(or_better=True))
 
 FEATURE_EXTRACTOR = 'brisk'  # 'sift'|'surf'|'brisk'|'orb'
 FEATURE_MATCHER = 'bf'  # 'bf'|'knn'
-GOOD_MATCH_PERCENT = 1 # Try 0.15
+NUM_GOOD_MATCHES = 200
 LOWES_RATIO = 0.75
-SCALING = 40 # Percent scale down
+SCALING = 100  # Percent scale down
+
 
 def get_args():
     """
@@ -33,14 +34,19 @@ def get_args():
                             help="path to directory to output combined images")
     arg_parser.add_argument("-s", "--scale", type=int, default=100,
                             help="Percentage of final scaled down image relative to original")
-    return arg_parser.parse_args()
+    args = vars(arg_parser.parse_args())
+
+    print('[ARGUMENTS LIST]')
+    for arg in args:
+        print('[ARG]{}:{}'.format(arg, args[arg]))
+    return args
 
 
 def load_images(folder_path):
     """
     Load all images from a folder
     """
-    image_paths = os.listdir(folder_path)
+    image_paths = sorted(os.listdir(folder_path))
     images_loc = []
     for image_rel_path in image_paths:
         image_path = folder_path + '/' + image_rel_path
@@ -90,7 +96,7 @@ def convert_to_grayscale(image):
 
 def get_descriptors(image):
     """
-    Fill documentation
+    Generate descriptors from image according to various descriptor modules
     """
     if FEATURE_EXTRACTOR == 'surf':
         descriptor = cv2.xfeatures2d.SURF_create()
@@ -107,7 +113,7 @@ def get_descriptors(image):
 
 def create_matcher():
     """
-    Fill documentation
+    Create different types of matchers to match descriptors.
     """
 
     cross_check = bool(FEATURE_MATCHER == 'bf')
@@ -121,7 +127,7 @@ def create_matcher():
 
 def average(lst):
     """
-    Fill documentation
+    Average x.distance from a list of x.
     """
     sum_ = 0
     for i in lst:
@@ -131,19 +137,19 @@ def average(lst):
 
 def get_matches(kp1_loc, dsc1_loc, kp2_loc, dsc2_loc):
     """
-    Fill documentation
+    Get the matching descriptors according to feature matcher.
     """
 
     if FEATURE_MATCHER == 'bf':
         raw_matches = MATCHER.match(dsc1_loc, dsc2_loc)
         raw_matches.sort(key=lambda x: x.distance)
-        # print("Brute Force #matches = ", len(raw_matches))
-        num_good_matches = int(len(raw_matches) * GOOD_MATCH_PERCENT)
-        matches_loc = raw_matches[:num_good_matches]
+        print("Brute Force #matches = ", len(raw_matches))
+        # num_good_matches = int(len(raw_matches) * GOOD_MATCH_PERCENT)
+        matches_loc = raw_matches[:NUM_GOOD_MATCHES]
 
     else:
         raw_matches = MATCHER.knnMatch(dsc1_loc, dsc2_loc, 2)
-        # print("KNN #matches = ", len(raw_matches))
+        print("KNN #matches = ", len(raw_matches))
         matches_loc = []
         for m_val, n_val in raw_matches:
             if m_val.distance < n_val.distance * LOWES_RATIO:
@@ -158,61 +164,76 @@ def get_matches(kp1_loc, dsc1_loc, kp2_loc, dsc2_loc):
 
     return average(matches_loc), points1_loc, points2_loc, matches_loc
 
-def warpTwoImages(img1, img2, H):
-    '''warp img2 to img1 with homograph H'''
-    h1,w1 = img1.shape[:2]
-    h2,w2 = img2.shape[:2]
-    pts1 = np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)
-    pts2 = np.float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
-    pts2_ = cv2.perspectiveTransform(pts2, H)
+
+def combine_images(img1_loc, img2_loc, h_val):
+    """
+    Warp img2 to img1 with homograph h_val
+    """
+
+    height1, width1 = img1_loc.shape[:2]
+    height2, width2 = img2_loc.shape[:2]
+    pts1 = np.array([[0, 0], [0, height1], [width1, height1], [width1, 0]],
+                    np.float32).reshape(-1, 1, 2)
+    pts2 = np.array([[0, 0], [0, height2], [width2, height2], [width2, 0]],
+                    np.float32).reshape(-1, 1, 2)
+    pts2_ = cv2.perspectiveTransform(pts2, h_val)
     pts = np.concatenate((pts1, pts2_), axis=0)
     [xmin, ymin] = np.int32(pts.min(axis=0).ravel() - 0.5)
     [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
-    t = [-xmin,-ymin]
-    Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # translate
+    t_val = [-xmin, -ymin]
+    h_translation = np.array(
+        [[1, 0, t_val[0]], [0, 1, t_val[1]], [0, 0, 1]])  # translate
 
-    result = cv2.warpPerspective(img2, Ht.dot(H), (xmax-xmin, ymax-ymin))
-    result[t[1]:h1+t[1],t[0]:w1+t[0]] = img1
-    return (result, Ht)
-
-# def warp_images(img1_loc, img2_loc, h_loc):
-#     """
-#     Fill documentation
-#     """
-#     rows1, cols1 = img1_loc.shape[:2]
-#     rows2, cols2 = img2_loc.shape[:2]
-#     print("0")
-
-#     list_of_points_1 = np.array(
-#         [[0, 0], [0, rows1], [cols1, rows1], [cols1, 0]], np.float32).reshape(-1, 1, 2)
-#     temp_points = np.array(
-#         [[0, 0], [0, rows2], [cols2, rows2], [cols2, 0]], np.float32).reshape(-1, 1, 2)
-#     print("1")
-
-#     list_of_points_2 = cv2.perspectiveTransform(temp_points, h_loc)
-#     list_of_points = np.concatenate(
-#         (list_of_points_1, list_of_points_2), axis=0)
-#     print(list_of_points)
-
-#     [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
-#     [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
-#     print("3")
-
-#     translation_dist = [-x_min, -y_min]
-#     h_translation = np.array(
-#         [[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
-#     print(((x_max - x_min, x_max, x_min), (y_max - y_min, y_max, y_min)))
-
-#     output_img = cv2.warpPerspective(
-#         img2_loc, h_translation.dot(h_loc), (x_max - x_min, y_max - y_min))
-#     output_img[translation_dist[1]:rows1+translation_dist[1],
-#                translation_dist[0]:cols1+translation_dist[0]] = img1_loc
-#     print("5")
-
-#     return output_img
+    result = cv2.warpPerspective(
+        img2_loc, h_translation.dot(h_val), (xmax-xmin, ymax-ymin))
+    result[t_val[1]:height1+t_val[1], t_val[0]:width1+t_val[0]] = img1_loc
+    return (result, h_translation)
 
 
-ARGS = vars(get_args())
+def get_relative_position(img_1, points_1, img_2, points_2):
+    """
+    Get relative positioning of two images.
+    """
+    height1, width1 = img_1.shape[:2]
+    avg1 = np.mean(points_1, axis=0)
+
+    height2, width2 = img_2.shape[:2]
+    avg2 = np.mean(points_2, axis=0)
+
+    print(avg1, avg2)
+
+    confidence = []
+
+    # 1 left and 2 right
+    confidence.append((avg1[0]/width1)*(1 - (avg2[0]/width2)))
+
+    # 1 right and 2 left
+    confidence.append((avg2[0]/width2)*(1 - (avg1[0]/width1)))
+
+    # 1 up and 2 down
+    confidence.append((avg1[1]/height1)*(1 - (avg2[1]/height2)))
+
+    # 1 down and 2 up
+    confidence.append((avg2[1]/height2)*(1 - (avg1[1]/height1)))
+
+    max_ind = np.argmax(confidence)
+
+    print(confidence)
+
+    if max_ind == 0:
+        return 'left', 'right', confidence[max_ind]
+
+    if max_ind == 1:
+        return 'right', 'left', confidence[max_ind]
+
+    if max_ind == 2:
+        return 'up', 'down', confidence[max_ind]
+
+    return 'down', 'up', confidence[max_ind]
+
+
+ARGS = (get_args())
+SCALING = ARGS['scale']
 
 print("[INFO] loading images...")
 IMAGE_GRP_FOLDERS = sorted(os.listdir(ARGS["images"]))
@@ -222,13 +243,14 @@ MATCHER = create_matcher()
 
 for image_grp in IMAGE_GRP_FOLDERS:
     final_image_name = image_grp+'.jpg'
-    print('[INFO] working on group - '+final_image_name)
+    print('[INFO] working on group - '+image_grp)
     images = load_images(ARGS['images']+'/'+image_grp)
     print("[INFO] Number of components = ", len(images))
-    images = [cv2.resize(x, (((x.shape[1])*SCALING)//100, (x.shape[0]*SCALING)//100)) for x in images]
+    if SCALING != 100:
+        images = [cv2.resize(x, (((x.shape[1])*SCALING)//100,
+                                 (x.shape[0]*SCALING)//100)) for x in images]
     grayscale_imgs = [convert_to_grayscale(x) for x in images]
     described_imgs = [get_descriptors(x) for x in grayscale_imgs]
-
     order = [i for i in range(len(images))]
     fin_hom = {}
     pair_hom = {}
@@ -242,15 +264,20 @@ for image_grp in IMAGE_GRP_FOLDERS:
         img2, img2_grayscale, img2_described = images[ind2], grayscale_imgs[ind2], described_imgs[ind2]
 
         (kp1, dsc1), (kp2, dsc2) = img1_described, img2_described
-        avg_distance, points1, points2, matches = get_matches(kp1, dsc1, kp2, dsc2)
+        avg_distance, points1, points2, matches = get_matches(
+            kp1, dsc1, kp2, dsc2)
         h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
         pair_hom[(ind1, ind2)] = (h, avg_distance, matches)
 
-    fin_hom[0] = np.identity(pair_hom[(0,1)][0].shape[0])
+        # loc1, loc2, conf = get_relative_position(img1, points1, img2, points2)
+        # print(loc1, loc2, conf)
+        display_image_with_matches(img1, kp1, img2, kp2, matches)
+
+    fin_hom[0] = np.identity(pair_hom[(0, 1)][0].shape[0])
 
     visited = [0]
-    for i in range(len(images)-1):
-        best_pair = (i,i)
+    for ind in range(len(images)-1):
+        best_pair = (ind, ind)
         max_num_matches = 0
         min_distance = 10000
         for j in visited:
@@ -267,7 +294,7 @@ for image_grp in IMAGE_GRP_FOLDERS:
                     # print(("indices: ", (ind11, ind21)))
                     if pair_hom[(ind11, ind21)][1] < min_distance or (len(pair_hom[(ind11, ind21)][2]) > max_num_matches and pair_hom[(ind11, ind21)][1] == min_distance):
                         best_pair = (ind11, ind21)
-                        # print("best_pair: ", best_pair)        
+                        # print("best_pair: ", best_pair)
                         max_num_matches = len(pair_hom[(ind11, ind21)][2])
                         min_distance = pair_hom[(ind11, ind21)][1]
         if best_pair[0] in visited:
@@ -278,34 +305,36 @@ for image_grp in IMAGE_GRP_FOLDERS:
             non_vis = best_pair[0]
 
         visited.append(non_vis)
-        print(best_pair)
-        print(visited)
-        print(fin_hom)
+        # print(best_pair)
+        # print(visited)
+        # print(fin_hom)
 
         if (non_vis, vis) in pair_hom:
-            fin_hom[non_vis] = np.dot(fin_hom[vis], pair_hom[(non_vis, vis)][0])
+            fin_hom[non_vis] = np.dot(
+                fin_hom[vis], pair_hom[(non_vis, vis)][0])
         else:
-            fin_hom[non_vis] = np.dot(fin_hom[vis], np.linalg.inv(pair_hom[(vis, non_vis)][0]))
+            fin_hom[non_vis] = np.dot(
+                fin_hom[vis], np.linalg.inv(pair_hom[(vis, non_vis)][0]))
 
     height, width, channels = images[0].shape
     fwidth = width
     fheight = height
     base = copy.deepcopy(images[0])
-    cum_trans = np.identity(pair_hom[(0,1)][0].shape[0])
+    cum_trans = np.identity(pair_hom[(0, 1)][0].shape[0])
     # trans[2][2] = 0
-    for i in range(1, len(images)):
+    for ind in range(1, len(images)):
         # Apply panorama correction
         # fwidth += width
         # fheight += height
         # img1 = cv2.warpPerspective(images[i], fin_hom[i], (fwidth, fheight))
-        base, trans = warpTwoImages(base, images[i], np.dot(cum_trans, fin_hom[i]))
+        base, trans = combine_images(
+            base, images[ind], np.dot(cum_trans, fin_hom[ind]))
         cum_trans = trans.dot(cum_trans)
         # img1[0:base.shape[0], 0:base.shape[1]] = base
         # base = copy.deepcopy(img1)
 
         plt.imshow(base)
         plt.show()
-
 
         # # T1 = np.matrix([[1., 0., 0. + width / 2],
         # #                   [0., 1., 0. + height / 2],
