55b4803a750461d3937897efb3a9de73398ad856
Shashwat Shivam <shivamshaswat@gmail.com>
2019-10-09 18:11:18 +0530

    Added callibration code

--- A3/code/callibration.py
+++ A3/code/callibration.py
@@ -1,44 +1,82 @@
-# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
-
-import glob
-
 import cv2
 import numpy as np
+from cv2 import aruco
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+markerLength = 0.25   # Here, our measurement unit is centimetre.
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = []  # 3d point in real world space
-imgpoints = []  # 2d points in image plane.
+def get_homography_from_corners(corners):
+    pts_dst = np.array([[corners[0][0], corners[0][1]],
+                        [corners[1][0], corners[1][1]],
+                        [corners[2][0], corners[2][1]],
+                        [corners[3][0], corners[3][1]]])
+    pts_src = np.array([[0, 0],
+                        [1, 0],
+                        [0, 1],
+                        [1, 1]])
 
-images = glob.glob("*.jpg")
+    h, status = cv2.findHomography(pts_src, pts_dst)
+    return h, status
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+# def get_camera_pose(h):
+#     h1 = h[:, 0]
+#     h2 = h[:, 1]
+#     h3 = np.cross(h1, h2)
 
-    # If found, add object points, image points (after refining them)
-    if ret is True:
-        objpoints.append(objp)
+#     val1 = np.linalg.norm(h1)
+#     val2 = np.linalg.norm(h2)
+#     tval = (val1+val2)/2
 
-        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners2)
+#     t = h[:, 2]/tval
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
-        cv2.imshow("img", img)
-        cv2.waitKey(500)
+#     return np.mat([h1, h2, h3, t])
 
-cv2.destroyAllWindows()
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-    objpoints, imgpoints, gray.shape[::-1], None, None
-)
+# # init video capture
+cap = cv2.VideoCapture(-1)
+while True:
+    ret, frame = cap.read()
+
+    size = frame.shape
+    focal_length = size[1]
+    center = (size[1]/2, size[0]/2)
+    camera_matrix = np.array(
+        [[focal_length, 0, center[0]],
+         [0, focal_length, center[1]],
+         [0, 0, 1]], dtype="double"
+    )
+
+    if not ret:
+        print("Unable to capture video")
+        exit(0)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(
+        gray, aruco_dict, parameters=parameters)
+
+    homography_dict = {}
+    camera_pose = {}
+
+    for ind, id_list in enumerate(ids):
+        id_ = id_list[0]
+        h, status = get_homography_from_corners(corners[id_])
+        homography_dict[id_] = h
+        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, markerLength,
+                                                        camera_matrix,
+                                                        dist_coeffs)
+
+        # camera_pose[id_] = get_camera_pose(h)
+        camera_pose[id_] = (rvec, tvec)
+
+    frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+
+    cv2.imshow("frame", frame_markers)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# cap.release()
+# cv2.destroyAllWindows()
