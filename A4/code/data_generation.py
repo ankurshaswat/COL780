65786677d176c7c170b7ec7c04ddaba811370294
Atishya-Jain <atishya.jain@rocketmail.com>
2019-11-12 23:49:58 +0530

    data generation changed

--- A4/code/data_generation.py
+++ A4/code/data_generation.py
@@ -2,9 +2,10 @@ import copy
 import sys
 import cv2
 import time
+import numpy as np
 
 if __name__ == "__main__":
-    VID_FEED = cv2.VideoCapture('./../videos/Pause.mp4')
+    VID_FEED = cv2.VideoCapture(sys.argv[2])
     i = 0
     while True:
         RET, FRAME = VID_FEED.read()
@@ -15,9 +16,10 @@ if __name__ == "__main__":
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
+        FRAME = np.rot90(FRAME, 3)
         cv2.imshow("frame", FRAME)
         # if i%10 == 0:
-        cv2.imwrite(sys.argv[1] + str(time.time())+".jpg", FRAME)
+        # cv2.imwrite(sys.argv[1] + str(time.time())+".jpg", FRAME)
         i += 1
     
     VID_FEED.release()
